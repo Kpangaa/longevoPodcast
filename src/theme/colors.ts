d9d0ba1c97517bd125f651de16984d416e506918
Hernan Moya <hernan.moya16@gmail.com>
2022-09-11 19:46:05 -0300

    [LONGEVO-3]: Se agrego el componente para los iconos y la paleta de colores

---
+++ src/theme/colors.ts
@@ -0,0 +1,25 @@
+export const primaryBlack = '#000000';
+export const primaryWhite = '#FFFFFF';
+
+export const opacityBlack05 = `${primaryBlack}0D`;
+export const opacityBlack20 = `${primaryBlack}32`;
+export const opacityBlack35 = `${primaryBlack}23`;
+export const opacityBlack80 = `${primaryBlack}CC`;
+export const opacityWhite80 = `${primaryWhite}CC`;
+export const opacityWhite70 = `${primaryWhite}B3`;
+export const opacityWhite30 = `${primaryWhite}4D`;
+export const opacityWhite12 = `${primaryWhite}1F`;
+export const opacityWhite00 = `${primaryWhite}00`;
+
+export const primaryBlueCrossDark = '#263144';
+export const secondaryYellowDark = '#FFC738';
+export const secondaryRedDark = '#FE5E5E';
+
+export const DarkBlueGradient = [primaryBlueCrossDark, primaryBlueCrossDark];
+
+export const opacityYellow20 = `${secondaryYellowDark}32`;
+
+export const transparent = 'transparent';
+export const translucent = 'rgba(0,0,0,0)';
+export const blackTransparent = 'rgba(0, 0, 0, 0.25)';
+export const whiteTransparent = 'rgba(255, 255, 255, 0.3)';
