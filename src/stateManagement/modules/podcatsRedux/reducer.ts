28ee87670dbeae50f6fd6ff9412d0d3faee94a1e
Kpanga <hernan.moya16@gmail.com>
2022-09-20 18:52:27 -0300

    [LONGEVO-10]: Se agrego los cambios para que se pueda reproducir los podcats

--- src/stateManagement/modules/podcatsRedux/reducer.ts
+++ src/stateManagement/modules/podcatsRedux/reducer.ts
@@ -1,23 +1,44 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-fallthrough */
-import {ADD_FAVORITE_PODCATS, PodcatsState, REMOVE_FAVORITE_PODCATS} from './types';
+import { PIApiEpisodeInfo } from '../../../interfaces/podcasts.interface';
+import {ADD_FAVORITE_PODCATS, IS_PLAY_PODCATS, IS_PLAY_PODCATS_ONE_TIME, ITEM_MUSIC_PODCATS, PLAY_PODCATS, PodcatsState, REMOVE_FAVORITE_PODCATS, VALUE_PLAY_PODCATS} from './types';
+import Sound from 'react-native-sound';
+
 
 const initialState: PodcatsState = {
     podcatsArray: [],
+    podcatsItemMp3: {} as PIApiEpisodeInfo,
+    isPlayPodcatsOneTime: false,
+    itemMusicPodcats: null as unknown as Sound,
+    isPlayPodcats: false,
+    valuePlayPodcats: 0,
 };
 
 const pokemonReducer = (state = initialState, action: any): PodcatsState => {
   switch (action.type) {
     case ADD_FAVORITE_PODCATS: {
       const { podcatsArray } = state;
-      console.log('action.payload.podcatsItem: ', action.payload.podcatsItem);
-      console.log('action.payload', action.payload);
       podcatsArray.push(action.payload.podcatsItem);
     }
     case REMOVE_FAVORITE_PODCATS: {
       const { podcatsArray } = state;
       const newArray = podcatsArray.filter((item) => item.id !== action.payload.idx);
       return { ...state, podcatsArray: newArray};
+    }
+    case PLAY_PODCATS: {
+      return { ...state, podcatsItemMp3: action.payload.podcatsItemPlay};
+    }
+    case IS_PLAY_PODCATS_ONE_TIME: {
+      return { ...state, isPlayPodcatsOneTime: action.payload.isPlayPodcatsOneTime};
+    }
+    case ITEM_MUSIC_PODCATS: {
+      return {...state, itemMusicPodcats: action.payload.itemMusicPodcats};
+    }
+    case IS_PLAY_PODCATS: {
+      return { ...state, isPlayPodcats: action.payload.isPlayPodcats};
+    }
+    case VALUE_PLAY_PODCATS: {
+      return { ...state, valuePlayPodcats: action.payload.valuePlayPodcats};
     }
     default:
       return state;
--- src/stateManagement/modules/podcatsRedux/types.ts
+++ src/stateManagement/modules/podcatsRedux/types.ts
@@ -1,12 +1,24 @@
 /* eslint-disable prettier/prettier */
-import {PIApiNewTrending} from '../../../interfaces/podcasts.interface';
+import {PIApiEpisodeInfo, PIApiNewTrending} from '../../../interfaces/podcasts.interface';
+import Sound from 'react-native-sound';
+
 
 export interface PodcatsState {
   podcatsArray: PIApiNewTrending[];
+  podcatsItemMp3: PIApiEpisodeInfo;
+  isPlayPodcatsOneTime: boolean;
+  itemMusicPodcats: Sound;
+  isPlayPodcats: boolean;
+  valuePlayPodcats: number;
 }
 
 export const ADD_FAVORITE_PODCATS = '@podcats/ADD_FAVORITE_PODCATS';
 export const REMOVE_FAVORITE_PODCATS = '@podcats/REMOVE_FAVORITE_PODCATS';
+export const PLAY_PODCATS = '@podcats/PLAY_PODCATS';
+export const IS_PLAY_PODCATS_ONE_TIME = '@podcats/IS_PLAY_PODCATS_ONE_TIME';
+export const ITEM_MUSIC_PODCATS = '@podcats/ITEM_MUSIC_PODCATS';
+export const IS_PLAY_PODCATS = '@podcats/IS_PLAY_PODCATS';
+export const VALUE_PLAY_PODCATS = '@podcats/VALUE_PLAY_PODCATS';
 
 interface AddFavoritePodcats {
   type: typeof ADD_FAVORITE_PODCATS;
@@ -18,6 +30,36 @@ interface RemoveFavoritePodcats {
   payload: {idx: number};
 }
 
+interface PlayPodcats {
+  type: typeof PLAY_PODCATS;
+  payload: {podcatsItemPlay: PIApiEpisodeInfo};
+}
+
+interface IsPlayPodcatsOneTime {
+  type: typeof IS_PLAY_PODCATS_ONE_TIME;
+  payload: {isPlayPodcatsOneTime: boolean};
+}
+
+interface ItemMusicPodcats {
+  type: typeof ITEM_MUSIC_PODCATS;
+  payload: {itemMusicPodcats: Sound};
+}
+
+interface IsPlayPodcats {
+  type: typeof IS_PLAY_PODCATS;
+  payload: {isPlayPodcats: boolean};
+}
+
+interface ValuePlayPodcats {
+  type: typeof VALUE_PLAY_PODCATS;
+  payload: {valuePlayPodcats: number};
+}
+
 export type PodcatsActionsTypes =
 | AddFavoritePodcats
-| RemoveFavoritePodcats;
+| RemoveFavoritePodcats
+| PlayPodcats
+| IsPlayPodcatsOneTime
+| ItemMusicPodcats
+| IsPlayPodcats
+| ValuePlayPodcats;
